032c2e5ebf8e118cb58acf58da6b592579dcd1c1
Navya-K-N <navyanreddy2002@gmail.com>
2023-04-21 14:40:50 +0530

    message

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sindhu N\workshop\campusworkshop\.venv
+set VIRTUAL_ENV=C:\Users\Sindhu N\workshop2\campusworkshop\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
